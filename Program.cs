d38170c4c0e5542aae69833ef73af49cc9399490
Tobias-David <tobidavid2005@gmail.com>
2025-10-12 17:12:54 +0200

    doneForDay1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,89 @@
-﻿Console.WriteLine("WOOOOOOOOOOWWWWWWWWWIEEEEEEEEEEEEEEEEEEEEEE");
+﻿Console.WriteLine("-----------------------------------------");
+Console.WriteLine("WOOOOOOOOOOWWWWWWWWWIEEEEEEEEEEEEEEEEEEEEEE");
 
 // wowie no way :O
+Console.WriteLine("-----------------------------------------");
+
+int a = 1;
+a = a + 1;
+Console.WriteLine(a);
+
+//Down casting!
+Console.WriteLine("-----------------------------------------");
+
+double b = 0.9;
+Console.WriteLine(b);
+int c = (int) b;
+Console.WriteLine(c);
+
+//increment and decrement
+Console.WriteLine("-----------------------------------------");
+
+int d = 2;
+Console.WriteLine(d);
+
+Console.WriteLine(--d);
+Console.WriteLine(d--);
+Console.WriteLine(d);
+Console.WriteLine(--d);
+Console.WriteLine(d--);
+
+Console.WriteLine(d);
+
+
+Console.WriteLine("-----------------------------------------");
+
+int x = 1;
+
+if (x >= 0)
+{
+    Console.WriteLine(1);
+}
+else
+{
+    Console.WriteLine(-1);
+}
+
+int y = (x>0 ? 1 : -1);
+Console.WriteLine(y);
+
+Console.WriteLine("---");
+
+//switch case break thing
+
+Console.WriteLine("-----------------------------------------");
+
+int e = 4;
+
+switch (e){
+    case 1:
+        Console.WriteLine("yipp");
+        break;
+    case 2:
+        Console.WriteLine("yippee");
+        break;
+    case 3: case 4:
+        Console.WriteLine("yippeeeeeeeeeeee");
+        break;
+    default:
+        Console.WriteLine("ERROR - SELF IMPLOSION IMINENT!");
+        break;
+}
+
+//arrays
+
+Console.WriteLine("-----------------------------------------");
+
+int[] months;
+
+months = new int[12];
+
+months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+
+
+
+
+
+
+
